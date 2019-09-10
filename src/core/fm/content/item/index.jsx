f582a1f1845ce34557e455d6dd5ff81b3addd986
Rocky <wpkpda@gmail.com>
2019-09-10 19:38:49 +0600

    fixed svg issue

--- src/core/fm/content/item/index.jsx
+++ src/core/fm/content/item/index.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
-import {remove_duplicate_slash} from "../../../Helper";
+import React, {Component} from 'react';
+import {remove_duplicate_slash} from '../../../Helper';
 
-const RAW = ["svg"];
-const IMAGE = ["jpg", "jpeg", "png", "gif"];
-const VIDEO = ["mkv", "mp4", "flv", "mpg", "mov"];
-const AUDIO = ["mp3", "aac", "ogg"];
+const RAW = ['svg'];
+const IMAGE = ['jpg', 'jpeg', 'png', 'gif'];
+const VIDEO = ['mkv', 'mp4', 'flv', 'mpg', 'mov'];
+const AUDIO = ['mp3', 'aac', 'ogg'];
 
 let TIMER = null;
 
@@ -14,15 +14,15 @@ export default class Item extends Component {
   }
 
   getClass = () => {
-    return "item " + this.props.className;
+    return 'item ' + this.props.className;
   };
 
   onClick = (e) => {
-    if(TIMER !== null) {
+    if (TIMER !== null) {
       return this.runDoubleClick();
     }
     const multi = e.ctrlKey;
-    TIMER = setTimeout(()=>this.runSingleClick(multi), 300);
+    TIMER = setTimeout(() => this.runSingleClick(multi), 300);
   };
 
   runSingleClick = (multi) => {
@@ -55,14 +55,15 @@ export default class Item extends Component {
   select = (multi) => {
     let item = this.props.item;
 
-    if(!multi) {
-      this.props.store.config.data.folders.forEach(folder=>{folder.selected=false;})
-      this.props.store.config.data.files.forEach(file=>{file.selected=false;})
+    if (!multi) {
+      this.props.store.config.data.folders.forEach(folder => {folder.selected = false;});
+      this.props.store.config.data.files.forEach(file => {file.selected = false;});
     }
 
     if (item.is_dir) {
       item = this.props.store.config.data.folders.find(x => x === item);
-    } else {
+    }
+    else {
       item = this.props.store.config.data.files.find(x => x === item);
     }
 
@@ -71,22 +72,22 @@ export default class Item extends Component {
 
   //run the callback with the result
   runCallback = item => {
-    let type = "unknown";
-    if(RAW.indexOf(item.extension.toLowerCase()) >= 0) {
+    let type = 'unknown';
+    if (RAW.indexOf(item.extension.toLowerCase()) >= 0) {
       return this.sendRawResult(item);
     }
     else if (IMAGE.indexOf(item.extension.toLowerCase()) >= 0) {
-      type = "image";
+      type = 'image';
     }
-    else if(VIDEO.indexOf(item.extension.toLowerCase()) >= 0) {
-      type = "video";
+    else if (VIDEO.indexOf(item.extension.toLowerCase()) >= 0) {
+      type = 'video';
     }
-    else if(AUDIO.indexOf(item.extension.toLowerCase()) >= 0) {
-      type = "audio";
+    else if (AUDIO.indexOf(item.extension.toLowerCase()) >= 0) {
+      type = 'audio';
     }
 
     const result = {
-      type
+      type,
     };
 
     result[`${type}`] = remove_duplicate_slash(this.props.store.working_dir + '/' + item.basename);
@@ -97,14 +98,14 @@ export default class Item extends Component {
   //send raw data as result
   sendRawResult = item => {
     this.props.store.httpGet(this.img())
-      .then(({data})=>{
-        const type = item.extension.toLowerCase();
-        const result = {
-          type
-        };
-        result[`${type}`] = data;
-        this.sendResult(result);
-      })
+        .then(({data}) => {
+          const type = item.extension.toLowerCase();
+          const result = {
+            type,
+          };
+          result[`${type}`] = data;
+          this.sendResult(result);
+        });
   };
 
   //call the callback and send the result
@@ -114,7 +115,7 @@ export default class Item extends Component {
         this.props.store.closeFileManager();
       }
     }
-    catch(e) {
+    catch (e) {
       console.log(e);
     }
   };
@@ -137,26 +138,31 @@ export default class Item extends Component {
         query.push('icon=' + ext);
       }
     }
-    if (path.indexOf('?') > -1)
+    if (path.indexOf('?') > -1) {
       return path + '&' + query.join('&');
+    }
     return path + '?' + query.join(('&'));
   };
 
   getMediaClass = () => {
-    const classes = ["fm-media"];
-    if(this.props.item.is_dir)
-      classes.push("fm-media--folder");
-    else
-      classes.push("fm-media--file");
-    if(this.props.item.selected)
-      classes.push("active");
-    return classes.join(" ");
+    const classes = ['fm-media'];
+    if (this.props.item.is_dir) {
+      classes.push('fm-media--folder');
+    }
+    else {
+      classes.push('fm-media--file');
+    }
+    if (this.props.item.selected) {
+      classes.push('active');
+    }
+    return classes.join(' ');
   };
 
   excerpt = () => {
     const basename = this.props.item.basename;
     const max = 11;
-    return basename.split('').length > max ? this.props.item.filename.substr(0, max) + '...' + ( this.props.item.is_dir ? '' : this.props.item.extension ) : basename;
+    return basename.split('').length > max ? this.props.item.filename.substr(0, max) + '...' +
+      (this.props.item.is_dir ? '' : this.props.item.extension) : basename;
   };
 
   render = () => {
@@ -164,7 +170,8 @@ export default class Item extends Component {
     let mediaTypeClass = ' ' + mediaType;
     return (
       <div className="fm-grid-m">
-        <div className={this.getMediaClass()} onDoubleClick={this.onDoubleClick} onClick={this.onClick} onContextMenu={this.props.onContextMenu}>
+        <div className={this.getMediaClass()} onDoubleClick={this.onDoubleClick} onClick={this.onClick}
+             onContextMenu={this.props.onContextMenu}>
           <div className="fm-media__thumb">
             <img src={this.img()} alt="icon"/>
           </div>
